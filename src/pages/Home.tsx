e60800cd2c00704f09179ed8c7d67b4a0025cd5e
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-26 23:42:24 +0600

    file updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,11 @@
-import Categories from "@/components/custom/Categories";
 import Contact from "@/components/custom/Contact";
-import Featured from "@/components/custom/Featured";
 import HeroSection from "@/components/custom/HeroSection";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
-      <Featured />
-      <Categories />
+
       <Contact />
     </div>
   );
