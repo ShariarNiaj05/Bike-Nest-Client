779e9a7f4388a68aa9bed105be7c77118c09d792
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-26 23:44:16 +0600

    route updated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,8 @@
-import UpdateProduct from "@/components/custom/UpdateProduct";
 import MainLayout from "@/layout/MainLayout";
 import About from "@/pages/About";
-import AllProduct from "@/pages/AllProduct";
-import Cart from "@/pages/Cart";
-import Checkout from "@/pages/Checkout";
+
 import Home from "@/pages/Home";
-import Manage from "@/pages/Manage";
-import PaymentSuccess from "@/pages/PaymentSuccess";
-import SingleProduct from "@/pages/SingleProduct";
-import SportsCategory from "@/pages/SportsCategory";
+
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
