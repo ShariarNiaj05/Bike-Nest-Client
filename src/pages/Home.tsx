ae34c39cff8e122e20a34e37543430a51cf977ee
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-27 00:10:08 +0600

    unused file and import remoed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
 import Contact from "@/components/custom/Contact";
-import HeroSection from "@/components/custom/HeroSection";
 
 const Home = () => {
   return (
     <div>
-      <HeroSection />
-
       <Contact />
     </div>
   );
