48d335f3853083aefe74c3580a81e79f3da5103d
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-28 00:41:14 +0600

    global type added

--- src/types/routes.type.ts
+++ src/types/routes.type.ts
@@ -1,5 +1,18 @@
 import { ReactNode } from "react";
 
+export type TRoute = {
+  path: string;
+  element: ReactNode;
+};
+
+export type TNavbarItem =
+  | {
+      key: string;
+      label: ReactNode;
+      children?: TNavbarItem[];
+    }
+  | undefined;
+
 export type TUserPath = {
   name?: string;
   path?: string;
