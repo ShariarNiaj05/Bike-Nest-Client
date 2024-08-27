01c04f0c24c28c0286cebec9754552f45845768b
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-28 00:29:04 +0600

    login and register sample page added

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/index.tsx";
+import router from "./routes/routes.tsx";
 import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,8 @@ import MainLayout from "@/layout/MainLayout";
 import About from "@/pages/About";
 
 import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 
 import { createBrowserRouter } from "react-router-dom";
 
@@ -18,6 +20,14 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
