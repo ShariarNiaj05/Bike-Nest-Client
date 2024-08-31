2c09f5cd9079f21281379c4f78703540c26eb648
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-01 01:13:12 +0600

    comming soon added

---
+++ src/components/shared/ComingSoon.tsx
@@ -0,0 +1,21 @@
+import Lottie from "react-lottie";
+import comingSoonAnimation from "../../../public/comming-soon.json";
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: comingSoonAnimation,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+const ComingSoon = () => {
+  return (
+    <div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </div>
+  );
+};
+
+export default ComingSoon;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Contact from "@/components/custom/Contact";
+import ComingSoon from "@/components/shared/ComingSoon";
 
 const Home = () => {
   return (
     <div>
       <Contact />
+      <ComingSoon />
     </div>
   );
 };
