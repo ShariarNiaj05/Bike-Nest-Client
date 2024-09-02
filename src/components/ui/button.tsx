ba5e1dbb5404dec2f03dbc6cc49cc54ee97fb0ba
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-03 01:09:16 +0600

    button style changed

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background text-black hover:bg-accent hover:text-accent-foreground",
+          "border border-primary bg-background text-primary hover:bg-primary hover:text-white",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import Contact from "@/components/custom/Contact";
 import ComingSoon from "@/components/shared/ComingSoon";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <div>
       <Contact />
       <ComingSoon />
+      <button>Delete</button>
+      <button>Add</button>
+
+      <Button variant={"destructive"}>Delete</Button>
+      <Button variant={"outline"}>Add</Button>
     </div>
   );
 };
