35dcf2e4da846be56d94f98d1409437d8b0ad289
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 16:10:36 +0600

    Title updated

--- src/components/custom/Title.tsx
+++ src/components/custom/Title.tsx
@@ -1,23 +1,23 @@
-// import { useEffect } from "react";
-// import AOS from "aos";
+import { useEffect } from "react";
+import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Title = ({ children }: { children: string }) => {
-  // useEffect(() => {
-  //   AOS.init();
-  //   AOS.refresh();
-  // }, []);
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <div
-    // data-aos="fade-up"
-    // data-aos-offset="200"
-    // data-aos-delay="50"
-    // data-aos-duration="1000"
-    // data-aos-easing="ease-in-out"
-    // data-aos-mirror="true"
-    // data-aos-once="false"
-    // data-aos-anchor-placement="top-center"
-    // className="relative border-b-8 border-primary ps-3 opacity-5"
+      data-aos="fade-up"
+      data-aos-offset="200"
+      data-aos-delay="50"
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-center"
+      className="relative border-b-8 border-primary ps-3 opacity-5"
     >
       <h2 className="text-6xl font-bold text-start text-primary">{children}</h2>
       <p className="absolute top-16 left-64 text-6xl md:text-4xl -z-10 font-semibold opacity-20">
