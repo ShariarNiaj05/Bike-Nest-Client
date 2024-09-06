54965744abfd28bfafb468f0ad2699e7c10bfe5b
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 21:42:56 +0600

    Dashboard layout

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -3,10 +3,20 @@ import { Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
   return (
-    <>
-      <SideBar />
-      <Outlet></Outlet>
-    </>
+    <div className="flex h-screen bg-gray-100">
+      {/* Sidebar */}
+      <aside className="w-64 bg-primary text-white flex-shrink-0">
+        <SideBar />
+      </aside>
+
+      {/* Main Content Area */}
+      <main className="flex-1 p-6 overflow-auto">
+        {/* Outlet to render content */}
+        <div className="bg-white rounded-lg shadow-lg p-6">
+          <Outlet />
+        </div>
+      </main>
+    </div>
   );
 };
 
