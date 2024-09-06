6d781172abc7427a1a47cc7881f8d46d8ea234e2
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 16:06:52 +0600

    testimonials section

--- src/components/custom/Testimonials.tsx
+++ src/components/custom/Testimonials.tsx
@@ -5,33 +5,6 @@ interface Testimonial {
   image?: string;
 }
 
-const testimonials: Testimonial[] = [
-  {
-    id: 1,
-    name: "John Doe",
-    review:
-      "Great bike rental service! The bikes were in top condition and the booking process was seamless.",
-    image:
-      "https://img.freepik.com/free-photo/young-male-posing-isolated-against-blank-studio-wall_273609-12356.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1725408000&semt=ais_hybrid",
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    review:
-      "Had an amazing experience riding through the city. Will definitely rent again!",
-    image:
-      "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg",
-  },
-  {
-    id: 3,
-    name: "Sam Wilson",
-    review:
-      "Affordable prices and excellent customer service. Highly recommend!",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR48kgQNS7jHuRO0_X6Qv5PCh-chkE0_E8uRA&s",
-  },
-];
-
 const Testimonials = () => {
   return (
     <section className="py-12 bg-white">
@@ -63,5 +36,32 @@ const Testimonials = () => {
     </section>
   );
 };
+
+const testimonials: Testimonial[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    review:
+      "Great bike rental service! The bikes were in top condition and the booking process was seamless.",
+    image:
+      "https://img.freepik.com/free-photo/young-male-posing-isolated-against-blank-studio-wall_273609-12356.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1725408000&semt=ais_hybrid",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    review:
+      "Had an amazing experience riding through the city. Will definitely rent again!",
+    image:
+      "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg",
+  },
+  {
+    id: 3,
+    name: "Sam Wilson",
+    review:
+      "Affordable prices and excellent customer service. Highly recommend!",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR48kgQNS7jHuRO0_X6Qv5PCh-chkE0_E8uRA&s",
+  },
+];
 
 export default Testimonials;
