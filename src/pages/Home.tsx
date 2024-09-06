c3a89685e90fe6035c81a0b4bc33fd18aa4955e0
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 17:48:10 +0600

    contact us

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,8 +15,8 @@ const Home = () => {
       <Testimonials />
       <WhyChooseUs />
       <CouponsAndDiscounts />
-
       <Contact />
+
       <ComingSoon />
       <button>Delete</button>
       <button>Add</button>
