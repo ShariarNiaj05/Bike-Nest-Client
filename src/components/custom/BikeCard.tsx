b5f914a1d987b80467b66baa32e9ff342eaa9c93
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 16:33:28 +0600

    type

--- src/components/custom/BikeCard.tsx
+++ src/components/custom/BikeCard.tsx
@@ -1,23 +1,26 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
+import { TBike } from "@/types";
 
-const BikeCard = ({ bike }) => {
+const BikeCard = ({ bike }: { bike: TBike }) => {
   const navigate = useNavigate();
 
-  const handleViewDetail = (id: number) => {
+  const handleViewDetail = (id: string) => {
     navigate(`/bikes/${id}`); // Redirect to bike detail page
   };
   return (
     <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center border border-primary transform transition-transform duration-300 hover:scale-105">
       <img
-        src={bike.image}
+        src={bike.imageUrl}
         alt={bike.brand}
         className="w-full h-48 object-cover rounded-md mb-4"
       />
       <h3 className="text-xl font-semibold mb-2">{bike.brand}</h3>
       <p className="text-gray-600 mb-4">Model: {bike.model}</p>
-      <p className="text-gray-800 font-bold mb-4">Price: ${bike.price}</p>
-      <Button onClick={() => handleViewDetail(bike.id)}>View Details</Button>
+      <p className="text-gray-800 font-bold mb-4">
+        Price: ${bike.pricePerHour}
+      </p>
+      <Button onClick={() => handleViewDetail(bike._id)}>View Details</Button>
     </div>
   );
 };
