d018130b87d134c1c9d0662abf8ba8e75ec2d3ee
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-07 01:20:30 +0600

    coming soon add single bike in page view

--- src/pages/admin/adminBikeManagement/AddSingleBike.tsx
+++ src/pages/admin/adminBikeManagement/AddSingleBike.tsx
@@ -1,5 +1,14 @@
+import ComingSoon from "@/components/shared/ComingSoon";
+
 const AddSingleBike = () => {
-  return <div>AddSingleBike.tsx</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <p className="text-2xl mb-8">
+        Add Single Bike in page view feature is on its way!
+      </p>
+      <ComingSoon />
+    </div>
+  );
 };
 
 export default AddSingleBike;
