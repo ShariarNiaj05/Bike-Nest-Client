479068842809fa77cd1286309aa131a5a61065e1
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 15:15:57 +0600

    file destination changed

--- src/components/custom/OurTeam.tsx
+++ src/components/custom/OurTeam.tsx
@@ -1,6 +1,6 @@
-import ceo from "/shariar.png";
-import cto from "/cto.jpg";
-import ho from "/ho.jpg";
+import ceo from "../../assets/shariar.png";
+import cto from "../../assets/cto.jpg";
+import ho from "../../assets/ho.jpg";
 const OurTeam = () => {
   return (
     <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8 text-center">
