60ad81a4938ea784c8a15291ebabc9663a446546
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 22:20:32 +0600

    add bulk bike

--- src/pages/admin/adminBikeManagement/AddBulkBike.tsx
+++ src/pages/admin/adminBikeManagement/AddBulkBike.tsx
@@ -1,5 +1,12 @@
+import ComingSoon from "@/components/shared/ComingSoon";
+
 const AddBulkBike = () => {
-  return <div>AddBulkBike.tsx</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <p className="text-2xl mb-8">The Add Bulk Bike feature is on its way!</p>
+      <ComingSoon />
+    </div>
+  );
 };
 
 export default AddBulkBike;
