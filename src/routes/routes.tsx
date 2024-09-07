3b0d5e69b796e3e7cd4fdd2a2ec0590dc6ab6776
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-07 23:04:43 +0600

    All  bike pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -27,6 +27,10 @@ const router = createBrowserRouter([
         path: "/about",
         element: <About />,
       },
+      {
+        path: "/bikes",
+        element: <About />,
+      },
     ],
   },
 
