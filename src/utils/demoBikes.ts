e7ceb5d3bb43621280bd844ccc111d9197775c36
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-08 00:54:02 +0600

    similar bike added

--- src/utils/demoBikes.ts
+++ src/utils/demoBikes.ts
@@ -42,4 +42,143 @@ export const bikesData: TBike[] = [
     description:
       "An advanced hybrid bike designed for fitness and adventure rides, offering excellent control.",
   },
+  {
+    _id: "4",
+    brand: "Trek",
+    model: "Marlin 7",
+    imageUrl:
+      "https://sklarbikes.com/cdn/shop/products/Adam-Sklar-and-His-Super-Something-Gravel-Bike-1.jpg?v=1708229654",
+    pricePerHour: 1400,
+    isAvailable: true,
+    cc: 350,
+    year: 2022,
+    name: "Trek Marlin 7",
+    description:
+      "A trail-worthy mountain bike, perfect for rugged terrains with a high-performance suspension fork.",
+  },
+  {
+    _id: "5",
+    brand: "Giant",
+    model: "Talon 2",
+    imageUrl:
+      "https://media.cnn.com/api/v1/images/stellar/prod/best-gravel-bikes-lead-cnnu.jpg?c=16x9&q=w_800,c_fill",
+    pricePerHour: 900,
+    isAvailable: false,
+    cc: 300,
+    year: 2021,
+    name: "Giant Talon 2",
+    description:
+      "Built for both adventure and daily commutes, the Talon 2 offers great comfort and control on all terrains.",
+  },
+  {
+    _id: "6",
+    brand: "Specialized",
+    model: "Rockhopper Comp",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/ride-mountain-bikes-raab-660c3f3e9a17c.jpg?crop=1xw:0.7501875468867217xh;center,top&resize=1200:*",
+    pricePerHour: 1600,
+    isAvailable: true,
+    cc: 400,
+    year: 2023,
+    name: "Specialized Rockhopper Comp",
+    description:
+      "A top-tier mountain bike built for challenging terrains with superior handling and control.",
+  },
+  {
+    _id: "7",
+    brand: "Cannondale",
+    model: "Trail 5",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLnCiI_lPFiuJzGNxdj0zQkmXH5yTT6jwLZw&s",
+    pricePerHour: 1100,
+    isAvailable: true,
+    cc: 275,
+    year: 2022,
+    name: "Cannondale Trail 5",
+    description:
+      "A versatile trail bike offering smooth handling and a reliable build for all types of riders.",
+  },
+  {
+    _id: "8",
+    brand: "Cannondale",
+    model: "Topstone Carbon 6",
+    imageUrl:
+      "https://c02.purpledshub.com/uploads/sites/39/2022/10/Fox-DHX-Factory-rear-mountain-bike-shock-2-3e0ee7f.jpg",
+    pricePerHour: 1700,
+    isAvailable: true,
+    cc: 450,
+    year: 2023,
+    name: "Cannondale Topstone Carbon 6",
+    description:
+      "A premium gravel bike with carbon frame, designed for speed and comfort on long-distance adventures.",
+  },
+  {
+    _id: "9",
+    brand: "Santa Cruz",
+    model: "Chameleon AL D",
+    imageUrl:
+      "https://i.etsystatic.com/22332666/r/il/19d92f/2399371443/il_570xN.2399371443_7vye.jpg",
+    pricePerHour: 1300,
+    isAvailable: false,
+    cc: 300,
+    year: 2022,
+    name: "Santa Cruz Chameleon AL D",
+    description:
+      "A dynamic trail bike designed for all-around performance, providing both agility and comfort on tough terrains.",
+  },
+  {
+    _id: "10",
+    brand: "Giant",
+    model: "Fathom 29 2",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJK2Lq_j9OVBDIXDAYmvHiyUyXiOVs0aSXfA&s",
+    pricePerHour: 1200,
+    isAvailable: true,
+    cc: 320,
+    year: 2022,
+    name: "Giant Fathom 29 2",
+    description:
+      "Built for both speed and control, the Fathom 29 2 excels on rugged trails with superior performance.",
+  },
+  {
+    _id: "11",
+    brand: "Trek",
+    model: "FX 3 Disc",
+    imageUrl:
+      "https://media.cnn.com/api/v1/images/stellar/prod/230419133455-velotric-thunder-1-ebike-lead-cnnu.jpg?c=original",
+    pricePerHour: 1200,
+    isAvailable: true,
+    cc: 250,
+    year: 2022,
+    name: "Trek FX 3 Disc",
+    description:
+      "A versatile hybrid bike with a lightweight frame, hydraulic disc brakes, and durable tires.",
+  },
+  {
+    _id: "12",
+    brand: "Giant",
+    model: "Escape 3",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-IwvCRddjz_yzsQMYz-A7tlGoBbztpcWLFdIYkh-BEBtrUpHCeDszeAXWvf4DpMU0wE4&usqp=CAU",
+    pricePerHour: 800,
+    isAvailable: false,
+    cc: 200,
+    year: 2021,
+    name: "Giant Escape 3",
+    description:
+      "A lightweight and durable bike, perfect for commuting and casual rides.",
+  },
+  {
+    _id: "13",
+    brand: "Specialized",
+    model: "Sirrus X 4.0",
+    imageUrl: "https://ride1up.com/wp-content/uploads/2024/06/3Q5A04482-2.webp",
+    pricePerHour: 1500,
+    isAvailable: true,
+    cc: 300,
+    year: 2023,
+    name: "Specialized Sirrus X 4.0",
+    description:
+      "An advanced hybrid bike designed for fitness and adventure rides, offering excellent control.",
+  },
 ];
