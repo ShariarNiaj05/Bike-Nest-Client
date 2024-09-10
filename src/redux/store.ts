92bacc914ff5d03c9451620054e865bfd497660b
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-11 02:16:34 +0600

    login user

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/baseApi";
-import authReducer from "../features/auth/authSlice";
-import { persistStore, persistReducer } from "redux-persist";
+import authReducer from "./features/authSlice";
+
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import thunk from "redux-thunk";
@@ -12,15 +23,32 @@ const persistConfig = {
   whitelist: ["auth"], // persist auth state
 };
 
-export const store = configureStore({
+const rootReducer = combineReducers({
+  auth: persistReducer(persistConfig, authReducer),
+  [baseApi.reducerPath]: baseApi.reducer,
+});
+
+/* export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
+}); */
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
