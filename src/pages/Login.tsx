a118aa0d256feabb76571327726304b714dcfa5a
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-13 00:47:16 +0600

    default value for semeless login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -50,8 +50,8 @@ const Login = () => {
               <input
                 type="email"
                 name="email"
-                defaultValue={defaultValue.email}
-                // value={email}
+                // value={defaultValue.email}
+                value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary"
                 required
@@ -67,8 +67,8 @@ const Login = () => {
               <input
                 type="password"
                 name="password"
-                defaultValue={defaultValue.password}
-                // value={password}
+                // value={defaultValue.password}
+                value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary"
                 required
