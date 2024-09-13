c447c99bcfd6c9974273e77bb0f6a92d36604cf7
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-13 16:14:39 +0600

    multiple file modification for login

--- src/redux/features/authSlice.ts
+++ src/redux/features/authSlice.ts
@@ -1,8 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
+export type TUser = {
+  _id: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  role: string;
+  createdAt: number;
+  updatedAt: number;
+  __v: number;
+};
+
 export interface AuthState {
-  user: any;
+  user: null | TUser;
   token: string | null;
 }
 
