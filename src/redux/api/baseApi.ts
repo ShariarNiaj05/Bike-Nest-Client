8d845f59a773f60ee470fce4cdea5f64227a07ad
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-13 19:13:06 +0600

    multiple file modification for login

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -72,52 +72,68 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-const baseQueryWithToken: BaseQueryFn<
-  FetchArgs,
-  BaseQueryApi,
-  DefinitionType
-> = async (args, api, extraOptions): Promise<any> => {
-  let result = await baseQuery(args, api, extraOptions);
+// const baseQueryWithToken: BaseQueryFn<
+//   FetchArgs,
+//   BaseQueryApi,
+//   DefinitionType
+// > = async (args, api, extraOptions): Promise<any> => {
+//   let result = await baseQuery(args, api, extraOptions);
 
-  /* if (result?.error?.status === 404) {
-    // toast.error(result?.error?.data?.message);
-    console.log("got 404 error in base api");
-  }
-  if (result?.error?.status === 403) {
-    // toast.error(result?.error?.data?.message);
-    console.log("got 403 error in base api");
-  } */
-  if (result?.error?.status === 401) {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
-      method: "POST",
-      credentials: "include",
-    });
+//   /* if (result?.error?.status === 404) {
+//     // toast.error(result?.error?.data?.message);
+//     console.log("got 404 error in base api");
+//   }
+//   if (result?.error?.status === 403) {
+//     // toast.error(result?.error?.data?.message);
+//     console.log("got 403 error in base api");
+//   } */
+//   if (result?.error?.status === 401) {
+//     const res = await fetch("http://localhost:5000/api/auth/login", {
+//       method: "POST",
+//       credentials: "include",
+//     });
 
-    const data = await res.json();
-    console.log("data in base api", data);
+//     const data = await res.json();
+//     console.log("data in base api", data);
 
-    if (data?.data?.accessToken) {
-      const user = (api.getState() as RootState).auth.user;
+//     if (data?.data?.accessToken) {
+//       const user = (api.getState() as RootState).auth.user;
 
-      api.dispatch(
-        setUser({
-          user,
-          token: data.data.accessToken,
-        })
-      );
+//       api.dispatch(
+//         setUser({
+//           user,
+//           token: data.data.accessToken,
+//         })
+//       );
 
-      result = await baseQuery(args, api, extraOptions);
-    } else {
-      api.dispatch(logout());
-    }
-  }
+//       result = await baseQuery(args, api, extraOptions);
+//     } else {
+//       api.dispatch(logout());
+//     }
+//   }
 
-  return result;
-};
+//   return result;
+// };
 
-export const baseApi = createApi({
+/* export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: baseQueryWithToken,
   tagTypes: ["bikes"],
+  endpoints: () => ({}),
+});
+ */
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000/api/v1",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("authorization", `${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: () => ({}),
 });
