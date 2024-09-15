ed30651f2e7bf3b103916566670d040246147161
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-15 21:33:27 +0600

    login page modification

--- src/components/custom/NavbarItem.tsx
+++ src/components/custom/NavbarItem.tsx
@@ -13,44 +13,6 @@ import { Link } from "react-router-dom";
 import React from "react";
 import { AuthState } from "@/redux/features/authSlice";
 
-/* const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-]; */
-
 const NavbarItem = ({ user }: { user: AuthState }) => {
   const { role } = user;
   return (
