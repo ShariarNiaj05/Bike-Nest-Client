4a1ba87df6edaad492f2b6890699fa6f2a48fccf
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-15 23:44:55 +0600

    dashboard sidebar modification

--- src/layout/ProtectedRoute.tsx
+++ src/layout/ProtectedRoute.tsx
@@ -1,29 +1,31 @@
 import {
+  logout,
   TUser,
   useCurrentToken,
   useCurrentUser,
 } from "@/redux/features/authSlice";
-import { useAppSelector } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: TProtectedRoute) => {
   const user: TUser | null = useAppSelector(useCurrentUser);
-  const token = useAppSelector(useCurrentToken);
-  console.log(user);
-  console.log(token);
-  const { role } = user;
-  if (!token) {
+  const dispatch = useAppDispatch();
+  // const token = useAppSelector(useCurrentToken);
+  // console.log(user);
+  // console.log(token);
+  const { role } = user || "";
+  /* if (!token) {
     return <Navigate to={"/login"} replace={true} />;
-  }
+  } */
   /* if (token) {
         user = 
     } */
 
-  /*   if (role !== undefined || role !== (user as TUser)?.role) {
+  if (role !== undefined || role !== (user as TUser)?.role) {
     dispatch(logout());
     return <Navigate to={"/login"} replace={true} />;
-  } */
+  }
   return children;
 };
 
