c98e34ebe98b3536c73ef82673e96349ba1a0e04
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-17 00:08:29 +0600

    get all my rentals bike

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,24 @@
+export const formatDate = (dateString: Date) => {
+  const date = new Date(dateString);
+
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+
+  // Adding the suffix (st, nd, rd, th) for the day
+  const dayWithSuffix =
+    day +
+    (day % 10 === 1 && day !== 11
+      ? "st"
+      : day % 10 === 2 && day !== 12
+      ? "nd"
+      : day % 10 === 3 && day !== 13
+      ? "rd"
+      : "th");
+
+  return `${dayWithSuffix} ${month}, ${year}`;
+};
+
+// Example usage:
+const dateStr = "2024-06-10T09:00:00.000Z";
+console.log(formatDate(dateStr)); // Output: 10th June, 2024
