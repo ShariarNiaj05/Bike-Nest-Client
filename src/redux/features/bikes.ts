9fa8c142d29c587be15a919a1d04223d590906d1
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-22 00:11:24 +0600

    bike details feature using redux query

--- src/redux/features/bikes.ts
+++ src/redux/features/bikes.ts
@@ -13,6 +13,7 @@ const bikes = baseApi.injectEndpoints({
     }),
     BikeDetails: builder.query({
       query: (id) => {
+        console.log("id in redux", id);
         return {
           url: `/bikes/${id}`,
           method: "GET",
