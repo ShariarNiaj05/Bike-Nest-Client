9ca930390b34270d9d830160a45d92ef547e4f38
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-21 23:14:52 +0600

    redux bike feature

--- src/components/custom/FeaturedSections.tsx
+++ src/components/custom/FeaturedSections.tsx
@@ -12,7 +12,7 @@ const FeaturedSections = () => {
           Available Bikes
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {bikes.map((bike) => (
+          {bikes?.map((bike) => (
             <BikeCard key={bike._id} bike={bike} />
           ))}
         </div>
