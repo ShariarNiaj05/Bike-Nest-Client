6408ca914f5f00e1046e2f336fc44a8b034e893c
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-24 01:01:28 +0600

    delete functionality by admin

--- src/redux/features/adminBike.ts
+++ src/redux/features/adminBike.ts
@@ -24,6 +24,7 @@ const adminBike = baseApi.injectEndpoints({
     }),
     deleteBike: builder.mutation({
       query: (id) => {
+        console.log("delete id", id);
         return {
           url: `/bikes/${id}`,
           method: "DELETE",
