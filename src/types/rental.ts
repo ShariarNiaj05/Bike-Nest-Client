b1f3dbc50678f2bcdb3ab5528597dd7a5ec878dc
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-27 23:15:06 +0600

    feat: return bike from admin dashboard

--- src/types/rental.ts
+++ src/types/rental.ts
@@ -2,6 +2,7 @@ import { TUser } from "@/redux/features/authSlice";
 import { TBike } from "./bike.type";
 
 export type TRental = {
+  _id: string;
   userId: string | TUser;
   bikeId: string | TBike;
   startTime: Date | string;
