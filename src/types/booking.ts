400b1284c1108c02c198c13ae30245d5ffd134ca
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-27 23:10:03 +0600

    feat: return bike from admin dashboard

--- src/types/booking.ts
+++ src/types/booking.ts
@@ -1,7 +1,7 @@
 import { TUser } from "@/redux/features/authSlice";
 import { TBike } from "./bike.type";
 
-export type TBooking = {
+export type TRental = {
   userId: string | TUser;
   bikeId: string | TBike;
   startTime: Date;
